1dfe17fcf92d56f27d851505d683c88b64e42070
Siddhesh M. Dhomse <siddheshdhomse2@gmail.com>
2026-03-29 23:00:02 +0530

    Day 3 Stack, Heap and String Operations

--- 05_DataType_Summary.js
+++ 05_DataType_Summary.js
@@ -1,1 +1,24 @@
-console.log("2" > 1);
+/*
+Primitive type(Stack memeory)
+
+String 
+number 
+boolean
+null
+undefined 
+bigint 
+symbol 
+
+Non Primitive Reference (Heap memory)
+Array
+Object
+Function
+
+Stack has copy of variable and orignal value is same 
+Heap shares orignal reference so changes the orignal value
+*/
+
+const myFunction = function(){
+    console.log("ok");
+    
+}
---
+++ 06_String.js
@@ -0,0 +1,18 @@
+const name = "siddhesh";
+const  age = 10;
+
+
+//console.log(`my name is ${name} and age is ${age}` );
+
+//console.log(name.toUpperCase());
+
+
+console.log(name.toUpperCase().substring(0,6));
+console.log(name.indexOf("s"));
+
+const subStr = name.substring(0,3);
+console.log(subStr);
+
+const mail = "hey-ok-okok";
+
+console.log(mail.split("-"));   
